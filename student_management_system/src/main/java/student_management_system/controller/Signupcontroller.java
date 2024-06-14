ecd8d4e330297636996cc7cd7dcbf756f6411d17
nandithapb23 <122531318+nandithapb23@users.noreply.github.com>
2024-06-14 12:13:31 +0530

    Add files via upload

---
+++ student_management_system/src/main/java/student_management_system/controller/Signupcontroller.java
@@ -0,0 +1,41 @@
+package student_management_system.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import student_management_system.dao.Studentdao;
+import student_management_system.dto.Student;
+
+@WebServlet("/signup")
+public class Signupcontroller extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
+		Student student = new Student();
+		student.setName(name);
+		student.setEmail(email);
+		student.setPassword(password);
+
+		Studentdao studentdao = new Studentdao();
+		Student student2 = studentdao.saveStudent(student);
+
+		if (student2 != null) {
+			req.setAttribute("message", "signedup succesfully");
+			RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
+			dispatcher.forward(req, resp);
+		} else {
+			RequestDispatcher dispatcher = req.getRequestDispatcher("signup.jsp");
+			dispatcher.include(req, resp);
+		}
+	}
+}
